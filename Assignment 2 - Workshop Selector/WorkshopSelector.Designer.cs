bade86059df37baad8d16703c7265574f48aff74
Daniel Romero <danieljromero305@gmail.com>
2024-08-21 19:39:46 -0400

    Added Assignment 2 - Workshop Selector

---
+++ Assignment 2 - Workshop Selector/WorkshopSelector.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Assignment_2___Workshop_Selector
+{
+    partial class WorkshopSelector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.WorkshopBox = new System.Windows.Forms.ListBox();
+            this.LocationBox = new System.Windows.Forms.ListBox();
+            this.ResultsField = new System.Windows.Forms.Label();
+            this.WorkshopLabel = new System.Windows.Forms.Label();
+            this.LocationLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.AutoSize = true;
+            this.CalculateButton.Location = new System.Drawing.Point(368, 207);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(110, 39);
+            this.CalculateButton.TabIndex = 0;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.AutoSize = true;
+            this.ExitButton.Location = new System.Drawing.Point(368, 252);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(110, 39);
+            this.ExitButton.TabIndex = 1;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // WorkshopBox
+            // 
+            this.WorkshopBox.FormattingEnabled = true;
+            this.WorkshopBox.Location = new System.Drawing.Point(12, 25);
+            this.WorkshopBox.Name = "WorkshopBox";
+            this.WorkshopBox.Size = new System.Drawing.Size(328, 108);
+            this.WorkshopBox.TabIndex = 2;
+            // 
+            // LocationBox
+            // 
+            this.LocationBox.FormattingEnabled = true;
+            this.LocationBox.Location = new System.Drawing.Point(355, 25);
+            this.LocationBox.Name = "LocationBox";
+            this.LocationBox.Size = new System.Drawing.Size(150, 108);
+            this.LocationBox.TabIndex = 3;
+            // 
+            // ResultsField
+            // 
+            this.ResultsField.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ResultsField.Location = new System.Drawing.Point(12, 136);
+            this.ResultsField.Name = "ResultsField";
+            this.ResultsField.Size = new System.Drawing.Size(328, 218);
+            this.ResultsField.TabIndex = 4;
+            // 
+            // WorkshopLabel
+            // 
+            this.WorkshopLabel.AutoSize = true;
+            this.WorkshopLabel.Location = new System.Drawing.Point(12, 9);
+            this.WorkshopLabel.Name = "WorkshopLabel";
+            this.WorkshopLabel.Size = new System.Drawing.Size(208, 13);
+            this.WorkshopLabel.TabIndex = 5;
+            this.WorkshopLabel.Text = "Workshop / No of Days / Registration Fee";
+            // 
+            // LocationLabel
+            // 
+            this.LocationLabel.AutoSize = true;
+            this.LocationLabel.Location = new System.Drawing.Point(355, 9);
+            this.LocationLabel.Name = "LocationLabel";
+            this.LocationLabel.Size = new System.Drawing.Size(123, 13);
+            this.LocationLabel.TabIndex = 6;
+            this.LocationLabel.Text = "Location / Lodging Fees";
+            // 
+            // WorkshopSelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 365);
+            this.Controls.Add(this.LocationLabel);
+            this.Controls.Add(this.WorkshopLabel);
+            this.Controls.Add(this.ResultsField);
+            this.Controls.Add(this.LocationBox);
+            this.Controls.Add(this.WorkshopBox);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.CalculateButton);
+            this.MaximumSize = new System.Drawing.Size(529, 404);
+            this.MinimumSize = new System.Drawing.Size(529, 404);
+            this.Name = "WorkshopSelector";
+            this.Text = "Workshop Selector";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label WorkshopLabel;
+        private System.Windows.Forms.Label LocationLabel;
+
+        private System.Windows.Forms.Label ResultsField;
+
+        private System.Windows.Forms.ListBox WorkshopBox;
+        private System.Windows.Forms.ListBox LocationBox;
+
+        private System.Windows.Forms.Button ExitButton;
+
+        private System.Windows.Forms.Button CalculateButton;
+
+        #endregion
+    }
+}
+
---
+++ Assignment 2 - Workshop Selector/WorkshopSelector.cs
@@ -0,0 +1,114 @@
+﻿// Programmer: Daniel Romero
+// Class: CIS 3000 Fall 2024
+// Program Purpose: To calculate the total price of registration and lodging for a specified workshop
+
+using System;
+using System.Windows.Forms;
+
+namespace Assignment_2___Workshop_Selector
+{
+    public partial class WorkshopSelector : Form
+    {
+        // Struct for workshop info to make data easier to parse without having to do string methods
+        private struct Workshops
+        {
+            public readonly string Name;
+            public readonly int Length;
+            public readonly int Price;
+
+            public Workshops(string name, int length, int price)
+            {
+                Name = name;
+                Length = length;
+                Price = price;
+            }
+        }
+
+        // Struct for Location info
+        private struct Locations
+        {
+            public readonly string Name;
+            public readonly int Price;
+
+            public Locations(string name, int price)
+            {
+                Name = name;
+                Price = price;
+            }
+        }
+
+        // defines workshop names, lengths in days, and prices
+        private readonly Workshops[] _workshops =
+        {
+            new Workshops("Handling Stress", 3, 1000),
+            new Workshops("Time Management", 3, 800),
+            new Workshops("Supervision Skills", 3, 1500),
+            new Workshops("Negotiation", 5, 1300),
+            new Workshops("How to Interview", 1, 500),
+        };
+
+        // defines locations and their prices per day
+        private readonly Locations[] _locations =
+        {
+            new Locations("Austin", 150),
+            new Locations("Chicago", 225),
+            new Locations("Dallas", 175),
+            new Locations("Orlando", 300),
+            new Locations("Phoenix", 175),
+            new Locations("Raleigh", 150),
+        };
+
+        // initializes program, adds workshops and locations into their respective info boxes
+        public WorkshopSelector()
+        {
+            InitializeComponent();
+
+            // Adds all workshop info into box
+            foreach (var workshop in _workshops)
+            {
+                WorkshopBox.Items.Add($"{workshop.Name}\t{workshop.Length} {DayPlural(workshop.Length)}\t${workshop.Price:N0}");
+            }
+
+            foreach (var location in _locations)
+            {
+                LocationBox.Items.Add($"{location.Name}\t${location.Price:N0}");
+            }
+        }
+
+        // simple method to return either 'day' or 'days' depending on the number of days
+        private static string DayPlural(int numOfDays)
+        {
+            if(numOfDays == 1) return "day";
+            return "days";
+        }
+
+        // Exit Button, closes program
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        // Runs calculation and puts it into the box
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            ResultsField.Text = "";
+            //Console.WriteLine($"{WorkshopBox.SelectedIndex} {LocationBox.SelectedIndex}");
+            if (WorkshopBox.SelectedIndex != -1 && LocationBox.SelectedIndex != -1)
+            {
+                var selectedWorkshop = _workshops[WorkshopBox.SelectedIndex];
+                var selectedLocation = _locations[LocationBox.SelectedIndex];
+
+                var locPrice = selectedLocation.Price;
+                var workPrice = selectedWorkshop.Price;
+                
+                ResultsField.Text += $"Registration: ${workPrice:N0}";
+                ResultsField.Text += $"\nLodging: ${locPrice:N0} \u00d7 {selectedWorkshop.Length} {DayPlural(selectedWorkshop.Length)} = ${(locPrice * selectedWorkshop.Length):N0}";
+                ResultsField.Text += $"\nTotal: ${(workPrice + (locPrice * selectedWorkshop.Length)):N0}";
+            }
+            else
+            {
+                ResultsField.Text = "Please select a workshop and location";
+            }
+        }
+    }
+}
