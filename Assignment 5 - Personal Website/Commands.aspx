5cdcbfd5a32cac7464a49ae74dcc7128975d8ff9
Daniel Romero <danieljromero305@gmail.com>
2024-11-18 18:46:51 -0500

    Completed Commands page

--- Assignment 5 - Personal Website/Commands.aspx
+++ Assignment 5 - Personal Website/Commands.aspx
@@ -5,9 +5,22 @@
 
     <main aria-labelledby="title">
         <h2 id="title"><%: Title %></h2>
-        <!-- <h3>Basic Git commands</h3> -->
-        Basic commands
-        <br>
-        <ul runat="server" id="commandList"></ul>
+        <section>
+            <h4>Basic Git commands</h4>
+            <ul runat="server" id="commandList"></ul>
+        </section>
+        <section>
+            <h4>.gitignore</h4>
+            <p>
+                You can define a <code>.gitignore</code> file in the root directory of your project.
+                Any file or folder names that you put into your <code>.gitignore</code> will be excluded from being added to the Git repository.
+            </p>
+            <p>
+                Usually, these are files that should not be uploaded to the internet or are unnecessary, or any files that are specific to your computer,
+                such as a file containing API keys or any files that have IDE settings like <code>.vs</code> or <code>.idea</code>.
+                Some other examples include folders that include the compiled files, usually a folder called <code>bin</code> or <code>release</code>, depending on language and folder structure.
+            </p>
+        </section>
+        <%-- TODO Add something about .gitignore and other hidden directories --%>
     </main>
 </asp:Content>
--- Assignment 5 - Personal Website/Commands.aspx.cs
+++ Assignment 5 - Personal Website/Commands.aspx.cs
@@ -12,7 +12,7 @@ namespace Assignment_5___Personal_Website
 
             public override string ToString()
             {
-                return $"<code>{Args}</code><br>&emsp;{Description}";
+                return $"<code>{Args}</code><br>{Description}";
             }
         }
 
@@ -25,13 +25,19 @@ namespace Assignment_5___Personal_Website
                 new("clone",
                     "This command creates a copy of an existing Git repository, whether it comes from GitHub, another computer, or any other source."),
                 new("pull", "This command pulls the latest changes from your remote repository, in our case GitHub."),
-                new("add", "This command "),
-                new("commit"),
-                new("status"),
-                new("push"),
-                new("reset"),
-                new("revert"),
-                new("branch"),
+                new("add",
+                    "This command adds the specified files to the Git repository. You can use <code>git add .</code> to add all files in the directory."),
+                new("commit",
+                    "This command commits all your changes to a commit, essentially a snapshot of the repository at that point in time."),
+                new("status",
+                    "This command shows the current status of the Git repository, including any pending commits to be uploaded to the remote. It also shows any pending changes that are not yet committed."),
+                new("push", "This command pushes the latest changes to the remote repository."),
+                new("reset",
+                    "This command resets any uncommited changes. Say you make a mistake in a certain file, this can be used to revert that specific file back to the last commit."),
+                new("revert",
+                    "This command allows you to revert any commits, whether they contain faulty or bad code or for any other reason. It differs from reset because reset changes files, revert changes commits."),
+                new("branch",
+                    "This command allows you to change branches, one of the most powerful features of Git. Branches allow you to keep multiple versions of the same code within the same repository, either to have multiple people work on the same code or to develop a version that may not need to be in the main branch. There are many different ways of using branches."),
             ];
 
             foreach (var command in listElems)
