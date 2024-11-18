39389bbf73c55a52bfb1fe2aef50a93c8c894d1c
Daniel Romero <danieljromero305@gmail.com>
2024-11-18 15:16:54 -0500

    Began Commands

--- Assignment 5 - Personal Website/Commands.aspx
+++ Assignment 5 - Personal Website/Commands.aspx
@@ -1,9 +1,13 @@
 ﻿<%@ Page Title="Commands" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="~/Commands.aspx.cs" Inherits="Assignment_5___Personal_Website.Commands" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <!-- TODO: Potentially add https://github.com/BattlefieldDuck/XtermBlazor in for an integrated terminal? could be cool -->
+
     <main aria-labelledby="title">
         <h2 id="title"><%: Title %></h2>
-        <h3>Your application description page.</h3>
-        <p>Use this area to provide additional information.</p>
+        <!-- <h3>Basic Git commands</h3> -->
+        Basic commands
+        <br>
+        <ul runat="server" id="commandList"></ul>
     </main>
 </asp:Content>
--- Assignment 5 - Personal Website/Commands.aspx.cs
+++ Assignment 5 - Personal Website/Commands.aspx.cs
@@ -1,17 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Assignment_5___Personal_Website
 {
     public partial class Commands : Page
     {
+        private readonly struct Command(string arg, string description = "")
+        {
+            private string Args { get; } = "git " + arg;
+            private string Description { get; } = description;
+
+            public override string ToString()
+            {
+                return $"<code>{Args}</code><br>&emsp;{Description}";
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            Command[] listElems =
+            [
+                new("init",
+                    "Use this to start a new Git repository! This should almost always be the first step!"),
+                new("clone",
+                    "This command creates a copy of an existing Git repository, whether it comes from GitHub, another computer, or any other source."),
+                new("pull", "This command pulls the latest changes from your remote repository, in our case GitHub."),
+                new("add", "This command "),
+                new("commit"),
+                new("status"),
+                new("push"),
+                new("reset"),
+                new("revert"),
+                new("branch"),
+            ];
 
+            foreach (var command in listElems)
+            {
+                commandList.InnerHtml += $"<li>{command.ToString()}</li>";
+            }
         }
     }
 }
